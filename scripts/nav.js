3be3f505c11d826d0eb91ce3b86d86280415ec35
DaTrueMeme <97398378+DaTrueMeme@users.noreply.github.com>
2026-02-11 21:45:50 -0600

    v12b
    Fixed theme mode event listener null reference error
    Raised scroll speed of gallery for mobile users
    Added highlight for the latest update

--- scripts/nav.js
+++ scripts/nav.js
@@ -10,8 +10,18 @@ function initializeNav() {
     const themeSwitch = document.getElementById("theme-switch");
 
     if (themeSwitch) {
+        if (localStorage.getItem("darkmode") === "active") {
+            document.body.classList.add("darkmode");
+        }
+
         themeSwitch.addEventListener("click", () => {
             document.body.classList.toggle("darkmode");
+
+            if (document.body.classList.contains("darkmode")) {
+                localStorage.setItem("darkmode", "active");
+            } else {
+                localStorage.setItem("darkmode", null);
+            }
         });
     }
 }
--- updates/script.js
+++ updates/script.js
@@ -2,10 +2,15 @@ fetch("updates.json")
     .then(res => res.json())
     .then(data => {
       const container = document.getElementById("updates");
+      latest_update = 1;
 
       data.forEach(update => {
         const updateDiv = document.createElement("div");
         updateDiv.classList.add("update");
+        if (latest_update === 1) {
+            updateDiv.classList.add("latest");
+            latest_update = 0;
+        }
 
         updateDiv.innerHTML = `
           <h2>v${update.version}</h2>
